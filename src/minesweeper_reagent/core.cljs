441fc1bac529c78f4cd70f512b9fd2bc7cbb43e8
Bobby Towers <porkostomus@mail.com>
2019-07-13 15:12:37 -0700

    use reagent cursors for better performance
    http://reagent-project.github.io/docs/master/ManagingState.html

--- src/minesweeper_reagent/core.cljs
+++ src/minesweeper_reagent/core.cljs
@@ -1,110 +1,188 @@
-(ns ^:figwheel-hooks minesweeper-reagent.core
+(ns ^:figwheel-hooks ms.core
   (:require
    [goog.dom :as gdom]
    [reagent.core :as reagent :refer [atom]]))
 
-(def grid-size (atom 12))
+(println "This text is printed from src/ms/core.cljs. Go ahead and edit it and see reloading in action.")
 
-(def num-mines (atom 18))
-
-(defn rand-positions []
-  (shuffle
-   (for [i (range @grid-size) j (range @grid-size)]
-     [i j])))
-
-(defn set-mines []
-  (for [i (range (* @grid-size @grid-size))]
-    {:mined (< i @num-mines)
-     :exposed false}))
+(defn multiply [a b] (* a b))
 
 (def app-state
-  (atom (into {} (map vector (rand-positions) (set-mines)))))
+  (atom {:width 15
+         :height 10
+         :percent-mines 20
+         :cells (set (for [x (range 15)
+                           y (range 10)]
+                       [x y]))
+         :mines #{}
+         :cleared #{}
+         :mouse-down? false
+         :flagged #{}}))
+
+(def cells (reagent/cursor app-state [:cells]))
+(def cleared (reagent/cursor app-state [:cleared]))
+(def mines (reagent/cursor app-state [:mines]))
+(def flagged (reagent/cursor app-state [:flagged]))
+
+(defn new-board! []
+  (swap! app-state assoc :cells
+         (set (for [x (range (:width @app-state))
+                    y (range (:height @app-state))]
+                [x y])))
+  (reset! mines #{})
+  (reset! cleared #{})
+  (reset! flagged #{}))
+
+(defn width-input []
+  [:label " Width: "
+   [:input
+    {:style {:width "6%"
+             :background-color "lightgray"}
+     :type "number"
+     :value (:width @app-state)
+     :on-change (fn [e] (swap! app-state assoc :width (-> e .-target .-value))
+                  (new-board!))}]])
+
+(defn height-input []
+  [:label "Height: "
+   [:input
+    {:style {:width "6%"
+             :background-color "lightgray"}
+     :type "number"
+     :value (:height @app-state)
+     :on-change (fn [e] (swap! app-state assoc :height (-> e .-target .-value))
+                  (new-board!))}]])
+
+(defn mines-input []
+  [:label " % mines"
+   [:input
+    {:style {:width "6%"
+             :background-color "lightgray"}
+     :type "number"
+     :value (:percent-mines @app-state)
+     :on-change (fn [e]
+                  (swap! app-state assoc
+                         :percent-mines (js/parseInt (-> e .-target .-value)))
+                  (new-board!))}]])
+
+(defn set-mines [[x y]]
+  (set (take (* (/ (:percent-mines @app-state) 100)
+                (* (:height @app-state) (:width @app-state)))
+             (shuffle (disj @cells [x y])))))
 
 (defn neighbors [[x y]]
-  (filter (partial contains? @app-state)
-          (for [dx [-1 0 1] dy [-1 0 1] :when (or dx dy)]
-            [(+ x dx) (+ y dy)])))
+  (filter #(contains? @cells %)
+  (for [dx [-1 0 1] dy [-1 0 1]
+        :when (or dx dy)]
+    [(+ x dx) (+ y dy)])))
 
 (defn mine-count [[x y]]
-  (if (:mined (get @app-state [x y])) 1 0))
+  (if (contains? @mines [x y]) 1 0))
 
 (defn mine-detector [[x y]]
   (reduce + (map (partial mine-count)
                  (neighbors [x y]))))
 
-(defn step [grid [x y]]
-  (let [square (get grid [x y])
-        step-square (assoc-in grid [[x y] :exposed] true)]
-    (cond
-      (:exposed square) grid
-      (or (:mined square) (< 0 (mine-detector [x y]))) step-square
-      :else (reduce step step-square (neighbors [x y])))))
+(defn step [state [x y]]
+  (cond
+    (contains? state [x y])
+    state
+    (or (contains? @mines [x y])
+        (< 0 (mine-detector [x y])))
+    (conj state [x y])
+    :else (reduce step (conj state [x y])
+                  (filter #(not (contains? state %))
+                  (neighbors [x y])))))
 
 (defn game-status []
   (cond
-    (some (fn [[_k v]] (and (:exposed v) (:mined v))) @app-state) :dead
-    (every? (fn [[_k v]] (or (:exposed v) (:mined v))) @app-state) :win
-    (some (fn [[_k v]] (:exposed v)) @app-state) :in-progress
+    (seq (clojure.set/intersection
+          @cleared @mines))
+    :dead
+    (every? #(or (contains? @cleared %)
+                 (contains? @mines %))
+            @cells)
+    :win
+    (seq @cleared)
+    :in-progress
     :else :new))
-
-(def mouse-down? (atom false))
 
 (defn icon []
   (case (game-status)
     :dead "🤯"
     :win "🤓"
-    (if @mouse-down? "😬" "🥺")))
-
-(defn flag! [[x y]]
-  (when (not (:exposed (get @app-state [x y])))
-  (swap! app-state assoc-in [[x y] :flagged] true)))
-
-(defn unflag! [[x y]]
-  (swap! app-state assoc-in [[x y] :flagged] false))
+    (if (:mouse-down? @app-state) "😬" "🥺")))
 
 (defn step! [[x y]]
-  (when (not (:flagged (get @app-state [x y])))
-    (reset! app-state (step @app-state [x y]))))
+  (when-not (contains? @flagged [x y])
+    (reset! cleared (step @cleared [x y]))))
 
-(defn rect-cell [[x y]]
-  (let [focused (atom false)]
-    (fn render-square []
-      [:rect
-       {:width 1.85 :height 1.85
-        :x -0.9 :y -0.9
-        :stroke-width (if @focused 0.1 0.08)
-        :stroke "black"
-        :fill (cond
-                (:exposed (get @app-state [x y])) "white"
-                @focused "darkgrey"
-                :else "silver")
-        :on-mouse-down
-        #(reset! mouse-down? true)
-        :on-mouse-up
-        #(reset! mouse-down? false)
-        :on-mouse-over
-        #(reset! focused true) 
-        :on-mouse-out
-        #(reset! focused false)
-        :on-click
-        #(when (not (:flagged (get @app-state [x y])))
-           (case (game-status)
-             :new
-             (do
-               (swap! app-state assoc [x y] {:mined false :exposed false})
-               (swap! app-state step [x y]))
-             :in-progress
-             (swap! app-state step [x y])))
-        :on-contextMenu
-        #(do (.preventDefault %)
-             (reset! mouse-down? false)
-             (cond
-               (:exposed (get @app-state [x y]))
-               (run! step! (neighbors [x y]))
-               (:flagged (get @app-state [x y]))
-               (unflag! [x y])
-               :else
-               (flag! [x y])))}])))
+(defn flag! [[x y]]
+  (when-not (contains? @cleared [x y])
+    (swap! flagged conj [x y])))
+
+(defn unflag! [[x y]]
+  (swap! flagged disj [x y]))
+
+(defn cell [status [x y]]
+  (let [focused? (atom false)]
+    (fn [status [x y]]
+      (if (= status :covered)
+        [:rect
+         {:width 1.85 :height 1.85
+          :x -0.9 :y -0.9
+          :stroke-width (if @focused? 0.1 0.08)
+          :stroke "black"
+          :fill (if @focused? "darkgrey" "silver")
+          :on-mouse-down
+          #(if-not (= (game-status) :dead)
+             (swap! app-state assoc :mouse-down? true))
+          :on-mouse-up
+          #(swap! app-state assoc :mouse-down? false)
+          :on-mouse-over
+          #(if-not (= (game-status) :dead)
+             (reset! focused? true))
+          :on-mouse-out
+          #(reset! focused? false)
+          :on-click
+          (fn [e] (when-not (or (contains? @flagged [x y])
+                                (= (game-status) :dead))
+                    (when (= (game-status) :new)
+                      (reset! mines (set-mines [x y])))
+                    (step! [x y])))
+          :on-contextMenu
+          #(do (.preventDefault %)
+               (swap! app-state assoc :mouse-down? false)
+               (if (contains? @flagged [x y])
+                 (unflag! [x y])
+                 (flag! [x y])))}]
+        [:rect
+         {:width 1.85 :height 1.85
+          :x -0.9 :y -0.9
+          :stroke-width 0.08
+          :stroke "black"
+          :fill "white"
+          :on-contextMenu
+          (fn [e]
+            (.preventDefault e)
+            (swap! app-state assoc :mouse-down? false)
+            (run! step! (neighbors [x y])))}]))))
+
+(defn flag [[x y]]
+  [:text
+   {:y 0.5 :text-anchor "middle" :font-weight "600" :fill "red" :font-size "1.5"
+    :on-contextMenu
+    #(do (.preventDefault %)
+         (swap! app-state assoc :mouse-down? false)
+         (if (contains? @flagged [x y]) (unflag! [x y])))}
+   "☠️"])
+
+(defn bomb []
+  [:text
+   {:y 0.5
+    :text-anchor "middle"
+    :font-size "1.5"}
+   "💥"])
 
 (defn mine-num [[x y]]
   [:text
@@ -115,83 +193,59 @@
             1 "blue" 2 "green" 3 "red" 4 "purple" 5 "brown" "black")
     :font-size "1.25"
     :on-mouse-down
-    #(do (reset! mouse-down? true)
-         (js/setTimeout (fn [] (reset! mouse-down? false)) 800))
+    #(do (swap! app-state assoc :mouse-down? true)
+         (js/setTimeout (fn [] (swap! app-state assoc :mouse-down? false)) 800))
     :on-contextMenu
     #(do (.preventDefault %)
-         (reset! mouse-down? true)
-         (js/setTimeout (fn [] (reset! mouse-down? false)) 800)
-         (run! step! (neighbors [x y])))}
+         (swap! app-state assoc :mouse-down? false)
+         (cond
+           (contains? @cleared [x y])
+           (run! step! (neighbors [x y]))
+           (contains? @flagged [x y])
+           (unflag! [x y])))}
    (mine-detector [x y])])
 
-(defn bomb []
-  [:text
-   {:y 0.5
-    :text-anchor "middle"
-    :font-size "1.5"}
-   "💥"])
-
-(defn flag [[x y]]
-      [:text
-       {:y 0.5 :text-anchor "middle" :font-weight "600" :fill "red" :font-size "1.5"
-        :on-contextMenu
-        #(do (.preventDefault %)
-             (reset! mouse-down? false)
-             (if (:flagged (get @app-state [x y])) (unflag! [x y])))}
-       "☠️" ])
-
 (defn render-board []
-  (into
-   [:svg.board
-    {:view-box (str "0 0 " @grid-size " " @grid-size)
-     :shape-rendering "auto"
-     :style {:max-height "500px"}}]
-   (for [[[x y] attrs] @app-state]
-     [:g {:transform (str "translate(" x  "," y ") "
-                          "scale (0.5)"
-                          "translate(1,1)")}
-      [rect-cell [x y]]
-      (if (:flagged attrs) [flag [x y]])
-      (when (:exposed attrs)
-        (if (:mined attrs)
-          [bomb]
-          (if (< 0 (mine-detector [x y]))
-            [mine-num [x y]])))])))
-
-(defn size-input []
-  (fn []
-      [:input {:style {:width "6%"
-                       :padding "4px 0;"
-                       :background-color "lightgray"}
-               :type "number"
-               :value @grid-size
-               :on-change #(do (reset! grid-size (-> % .-target .-value))
-                               (reset! app-state (into {} (map vector (rand-positions) (set-mines)))))}]))
-
-(defn mines-input []
-  (fn []
-      [:input {:style {:width "6%"
-                       :padding "4px 0;"
-                       :background-color "lightgray"}
-               :type "number"
-               :value @num-mines
-               :on-change #(do (reset! num-mines (-> % .-target .-value))
-                               (reset! app-state (into {} (map vector (rand-positions) (set-mines)))))}]))
+  [:svg
+   {:view-box (str "0 0 " (:width @app-state) " " (:height @app-state))
+    :shape-rendering "auto"
+    :style {:max-height "500px"}}
+   (doall
+    (for [[x y] @cells]
+      ^{:key [x y]}
+      [:g {:transform (str "translate(" x  "," y ") "
+                           "scale (0.5)"
+                           "translate(1,1)")}
+       (if (contains? @cleared [x y])
+         [cell :cleared [x y]])
+       (if (< 0 (mine-detector [x y]))
+         [mine-num [x y]])
+       (if (contains? @mines [x y])
+         [bomb])
+       (when-not (contains? @cleared [x y])
+         [cell :covered [x y]])
+       (if (contains? @flagged [x y])
+         [flag [x y]])]))])
 
 (defn minesweeper []
   [:center
-   [:h1 "Minesweeper"]
-   [:div "Size: " [size-input] " Mines: " [mines-input]]
-   [:p ]
+      [:div {:style {:font-size "14px"}} [height-input] [width-input] [mines-input]]
+   [:p]
    [:div
     {:style {:font-size "75px"}
-     :on-click #(reset! app-state (into {} (map vector (rand-positions) (set-mines))))}
+     :on-click #(new-board!)}
     [icon]]
    [render-board]])
 
+(defn get-app-element []
+  (gdom/getElement "app"))
+
+(defn mount [el]
+  (reagent/render-component [minesweeper] el))
+
 (defn mount-app-element []
-  (when-let [el (gdom/getElement "app")]
-    (reagent/render-component [minesweeper] el)))
+  (when-let [el (get-app-element)]
+    (mount el)))
 
 ;; conditionally start your application based on the presence of an "app" element
 ;; this is particularly helpful for testing this ns without launching the app
@@ -203,4 +257,4 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-  )
+)
